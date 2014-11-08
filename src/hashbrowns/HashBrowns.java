251b97a1874d282b096be56a440bba2eb0399ce6
Laura Barreto <labarreto@vassar.edu>
2014-11-08 18:00:57 -0500

    this implementation uses recursion.

--- src/hashbrowns/HashBrowns.java
+++ src/hashbrowns/HashBrowns.java
@@ -6,15 +6,87 @@
 
 package hashbrowns;
 
-/**
- *
- * @author ldbruby95
- */
 public class HashBrowns {
-
+    
     /**
-     * @param args the command line arguments
+     * 
+     * @param <K> key
+     * @param <V> value
      */
+    
+    interface HashMappy<K extends Comparable, V> {
+        V lookup (K k, V value);
+        HashMappy<K,V> add(K k, V v);
+    }
+    
+    class HashyMT<K extends Comparable, V> implements HashMappy<K,V> {
+        public HashyMT() {}
+        public V lookup(K key, V value) {
+            return value;
+        }
+        public HashMappy<K,V> add(K key, V value) {
+            return this; //adding a value, so you want to return a new HashMap
+            //with the K and V, on this. 
+        }
+        
+        
+    }
+    
+    class Hashy<K extends Comparable, V> implements HashMappy<K,V> {
+        K coolkey;
+        V coolvalue;
+        Hashy<K,V> next;
+        
+        public Hashy(K coolkey, V coolvalue, Hashy<K,V> next) {
+            this.coolkey = coolkey;
+            this.coolvalue = coolvalue;
+            this.next = next;
+        }
+
+
+        public V lookup(K k, V value) {
+           if (coolkey.compareTo(k) == 0){
+               return value;
+           } else {
+               return this.next.lookup(k, value);
+           }
+        }
+
+      
+        public HashMappy<K, V> add(K k, V v) {
+            return new Hashy<K,V>(k, v, this);
+        }
+        
+    }
+    interface HashMap<K extends Comparable, V> {
+        
+        //returning a value after looking up a key and it's value
+        V lookup(K key, V def);
+        //returns a map after you add a key and a value
+        void add( K key, V value);
+    }
+   
+    
+
+    class HashBrown<K extends Comparable, V> implements HashMap<K,V> {
+        Object data[];
+        
+        public HashBrown(int size) {
+            this.data = new Object[size];
+            HashMap<K,V> empty;
+            
+        }
+        
+        public V lookup (K key, V value) {
+            
+        }
+        
+        public void add (K key, V value) {
+            
+        }
+    }
+    
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
